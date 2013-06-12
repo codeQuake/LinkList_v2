f1f38d716c79c375cd836c8b3a054babd2bcc34e
Jens Krumsieck <info@codequake.de>
2013-06-12 20:54:05 +0200

    linkStats

---
+++ files/lib/system/event/listener/StatsSidebarDashboardBoxListener.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace linklist\system\event\listener;
+use linklist\system\cache\builder\LinklistStatsCacheBuilder;
+use wcf\system\event\IEventListener;
+use wcf\system\WCF;
+
+class StatsSidebarDashboardBoxListener implements IEventListener {
+
+    public function execute($eventObj, $className, $eventName) {
+        WCF::getTPL()->assign(array(
+            'linklistStats' => LinklistStatsCacheBuilder::getInstance()->getData()
+        ));
+    }
+}
---
+++ templates/__dashboardBoxStatsSidebar.tpl
@@ -0,0 +1,4 @@
+{if $linklistStats|isset}
+	<dt>{lang}linklist.link.links{/lang}</dt>
+	<dd>{#$linklistStats[links]}</dd>
+{/if}
