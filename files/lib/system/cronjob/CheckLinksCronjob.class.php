7b28dce64fe77aa3b0c4b7311234503e2a203075
Jens Krumsieck <info@codequake.de>
2013-08-27 14:10:07 +0200

    using curl now

--- files/lib/system/cronjob/CheckLinksCronjob.class.php
+++ files/lib/system/cronjob/CheckLinksCronjob.class.php
@@ -18,27 +18,30 @@ class CheckLinksCronjob extends AbstractCronjob{
             $statement->execute(array(0));
             while($row = $statement->fetchArray()){
                 $link = new Link($row['linkID']);
-                ini_set('user_agent', 'Mozilla/5.0 (compatible; codeQuake Linklist V2.0 +http://codequake.de)');
-                @$headers = get_headers($link->url);
-                if($headers != false){
-                $code = substr($headers[0], 9, 3);
-                    if(intval($code) < 400){
-                        $editor = new LinkEditor($link);
-                        $editor->update(array(
+                //ini_set('user_agent', 'Mozilla/5.0 (compatible; codeQuake Linklist V2.0 +http://codequake.de)');
+                //@$headers = get_headers($link->url);
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $link->url);
+                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0');
+                curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch,CURLOPT_VERBOSE,false);
+                curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch,CURLOPT_SSLVERSION,3);
+                curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, false);
+                $page = curl_exec($ch);
+                $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                curl_close($ch);
+                if($code >=200 && $code < 400){
+                     $editor = new LinkEditor($link);
+                     $editor->update(array(
                                 'isOnline' => 1
-                         ));
-                    }
-                else{
-                    $editor = new LinkEditor($link);
-                    $editor->update(array(
-                            'isOnline' => 0
                      ));
                 }
-                }
                 else{
-                    $editor = new LinkEditor($link);
-                    $editor->update(array(
-                            'isOnline' => 0
+                     $editor = new LinkEditor($link);
+                     $editor->update(array(
+                                'isOnline' => 0
                      ));
                 }
                 
