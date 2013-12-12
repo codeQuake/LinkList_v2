9833cffa2be77f4971f30ba698faecf89c8655f7
Jens Krumsieck <info@codequake.de>
2013-12-12 18:33:36 +0100

    fixed user rights in dashboardlists

--- files/lib/data/link/ViewableLinkList.class.php
+++ files/lib/data/link/ViewableLinkList.class.php
@@ -5,5 +5,22 @@ use linklist\data\link\LinkList;
 
 class ViewableLinkList extends LinkList{
     public $decoratorClassName = 'linklist\data\link\ViewableLink';
-
+    
+    public function __construct(){
+        parent::__construct();
+        $linkIDs = $this->getLinks();
+        $accessibleLinkIDs = implode(",",$linkIDs);
+        $this->getConditionBuilder()->add('linkID IN (?)', array($accessibleLinkIDs));
+    }
+    
+    protected function getLinks(){
+        $linkIDs = array();
+        $list = new LinkList();
+        $list->readObjects();
+        $list = $list->getObjects();
+        foreach($list as $item){
+            if($item->isVisible()) $linkIDs[] = $item->linkID;
+        }
+        return $linkIDs;
+     }
 }
