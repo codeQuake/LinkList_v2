f8e0dd4add364e91cda3da4f078dccffecabc5ea
Jens Krumsieck <info@codequake.de>
2013-12-12 18:26:28 +0100

    fixed dashboard box

--- files/lib/system/dashboard/box/RandomLinkDashboardBox.class.php
+++ files/lib/system/dashboard/box/RandomLinkDashboardBox.class.php
@@ -3,6 +3,7 @@ namespace linklist\system\dashboard\box;
 use wcf\data\dashboard\box\DashboardBox;
 use wcf\page\IPage;
 use linklist\data\link\Link;
+use linklist\data\link\ViewableLinkList;
 use wcf\system\dashboard\box\AbstractContentDashboardBox;
 use wcf\system\WCF;
 
@@ -12,13 +13,16 @@ class RandomLinkDashboardBox extends AbstractContentDashboardBox{
     
     public function init(DashboardBox $box, IPage $page) {
      parent::init($box, $page);
-        $sql = "SELECT linkID FROM linklist".WCF_N."_link ORDER BY RAND() LIMIT 1";
-        $statement = WCF::getDB()->prepareStatement($sql);
-        $statement->execute();
-        $row = $statement->fetchArray();
+        $linkIDs = $this->getLinks();
+        if(!empty($linkIDs)){
+            $accessibleLinkIDs = implode(",",$linkIDs);
+            $sql = "SELECT linkID FROM linklist".WCF_N."_link WHERE linkID IN (?) ORDER BY RAND() LIMIT 1";
+            $statement = WCF::getDB()->prepareStatement($sql);
+            $statement->execute(array($accessibleLinkIDs));
+            $row = $statement->fetchArray();
         
-        $this->link = new Link($row['linkID']);
-        if(!$this->link->isVisible()) $this->init($box, $page);
+            $this->link = new Link($row['linkID']);
+        }
      }
      
      protected function render(){
@@ -28,6 +32,17 @@ class RandomLinkDashboardBox extends AbstractContentDashboardBox{
             ));
             return WCF::getTPL()->fetch('dashboardBoxRandomLink', 'linklist');
             }
+     }
+     
+     protected function getLinks(){
+        $linkIDs = array();
+        $list = new ViewableLinkList();
+        $list->readObjects();
+        $list = $list->getObjects();
+        foreach($list as $item){
+            if($item->isVisible()) $linkIDs[] = $item->linkID;
+        }
+        return $linkIDs;
      }
     
 }
