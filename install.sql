feb8ba38cd7f0fdea7dff05119c16dd0ace34c34
Jens Krumsieck <info@codequake.de>
2014-07-13 17:18:29 +0200

    started development for version 2.1

--- install.sql
+++ install.sql
@@ -1,45 +1,46 @@
--- add links in user table
-ALTER TABLE wcf1_user ADD linklistLinks INT(10) NOT NULL DEFAULT 0;
-ALTER TABLE wcf1_user ADD INDEX linklistLinks (linklistLinks);
-
 --links
 DROP TABLE IF EXISTS linklist1_link;
 CREATE TABLE linklist1_link (
-linkID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-categoryID INT(10),
-userID	INT(10),
-username	VARCHAR(255),
-subject	VARCHAR(255) NOT NULL,
-message	TEXT,
-url	VARCHAR(255),
-image	VARCHAR(255) DEFAULT NULL,
-time INT(10) NOT NULL,
-languageID INT(10),
-attachments INT(10) NOT NULL DEFAULT 0,
-isActive	TINYINT(1) NOT NULL DEFAULT 0,
-isDeleted	TINYINT(1) NOT NULL DEFAULT 0,
-isOnline	TINYINT(1) NOT NULL DEFAULT 1,
-visits	INT(20)	NOT NULL DEFAULT 0,
-deleteTime INT(10) NULL,
-lastChangeTime	INT(10),
-enableSmilies TINYINT(1) NOT NULL DEFAULT 1,
-enableHtml TINYINT(1) NOT NULL DEFAULT 0,
-enableBBCodes	TINYINT(1) NOT NULL DEFAULT 1,
-ipAddress VARCHAR(39) NOT NULL DEFAULT '',
-cumulativeLikes MEDIUMINT(7) NOT NULL DEFAULT 0,
-hasLabels INT(10) NOT NULL DEFAULT 0;
+	linkID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	userID	INT(10),
+	username	VARCHAR(255),
+	subject	VARCHAR(255) NOT NULL,
+	teaser MEDIUMTEXT,
+	message	TEXT,
+	url	VARCHAR(255),
+	image	VARCHAR(255) DEFAULT NULL,
+	time INT(10) NOT NULL,
+	attachments INT(10) NOT NULL DEFAULT 0,
+	isDeleted	TINYINT(1) NOT NULL DEFAULT 0,
+	isDisabled	TINYINT(1) NOT NULL DEFAULT 0,
+	lastChangeTime INT(10) NOT NULL DEFAULT 0,
+	lastEditor VARCHAR (255) NOT NULL DEFAULT '',
+	lastEditorID INT(10) NOT NULL DEFAULT 0,
+	clicks	INT(20)	NOT NULL DEFAULT 0,
+	visits	INT(20)	NOT NULL DEFAULT 0,
+	deleteTime INT(10) NULL,
+	lastChangeTime	INT(10),
+	enableSmilies TINYINT(1) NOT NULL DEFAULT 1,
+	enableHtml TINYINT(1) NOT NULL DEFAULT 0,
+	enableBBCodes	TINYINT(1) NOT NULL DEFAULT 1,
+	ipAddress VARCHAR(39) NOT NULL DEFAULT '',
+	cumulativeLikes MEDIUMINT(7) NOT NULL DEFAULT 0
 );
 
---stats
-DROP TABLE IF EXISTS linklist1_category_stats;
-CREATE TABLE linklist1_category_stats(
-categoryID INT(10),
-links INT(10) DEFAULT 0,
-visits INT(10) DEFAULT 0
+--link to category
+DROP TABLE IF EXISTS linklist1_link_to_category;
+CREATE TABLE linklist1_link_to_category (
+	categoryID INT(10) NOT NULL,
+	linkID INT(10) NOT NULL,
+
+	PRIMARY KEY (categoryID, linkID)
 );
 
 --foreigns
+ALTER TABLE wcf1_user ADD linklistLinks INT(10) NOT NULL DEFAULT 0;
+ALTER TABLE wcf1_user ADD INDEX linklistLinks (linklistLinks);
 ALTER TABLE linklist1_category_stats ADD FOREIGN KEY (categoryID) REFERENCES wcf1_category (categoryID) ON DELETE CASCADE;
-ALTER TABLE linklist1_link ADD FOREIGN KEY (categoryID) REFERENCES wcf1_category (categoryID) ON DELETE CASCADE;
 ALTER TABLE linklist1_link ADD FOREIGN KEY (userID) REFERENCES wcf1_user (userID) ON DELETE SET NULL;
 ALTER TABLE linklist1_link ADD FOREIGN KEY (languageID) REFERENCES wcf1_language (languageID) ON DELETE SET NULL;
+ALTER TABLE linklist1_link_to_category ADD FOREIGN KEY (categoryID) REFERENCES wcf1_category (categoryID) ON DELETE CASCADE;
+ALTER TABLE linklist1_link_to_category ADD FOREIGN KEY (linkID) REFERENCES linklist1_link (newsID) ON DELETE CASCADE;
